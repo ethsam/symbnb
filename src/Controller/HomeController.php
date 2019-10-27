ae5a652476cc225eb5ae8eaf15b36bd1191e8584
Samuel <etheve.samuel@gmail.com>
2019-10-27 09:00:56 +0400

    assets

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,16 +1,18 @@
 <?php
 namespace App\Controller;
-use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="Home")
      */
     public function home()
     {
-        return new response("Hello world");
+        return $this->render('home/home.html.twig');
     }
 
 }
